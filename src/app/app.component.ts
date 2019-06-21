66ab3dc59b797ec679308ceca54b61e75c988792
Luiz <argentinaluiz@gmail.com>
2019-06-21 18:41:38 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Facebook, FacebookLoginResponse} from '@ionic-native/facebook/ngx';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fb: Facebook
   ) {
     this.initializeApp();
   }
@@ -21,6 +23,10 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.fb.login(['public_profile', 'user_friends', 'email'])
+          .then((res: FacebookLoginResponse) => alert('Logged into Facebook!'))
+          .catch(e => console.log('Error logging into Facebook', e));
     });
   }
 }
